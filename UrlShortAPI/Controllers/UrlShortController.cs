9f55074a85e7265bfb89501af512fb4d5db7e5a0
GustavoSelhorstMarconi <gustavo.selmarconi@gmail.com>
2025-11-21 09:54:49 -0300

    chore(ShortUrl): Added docs and renamed variables.

--- UrlShortAPI/Controllers/UrlShortController.cs
+++ UrlShortAPI/Controllers/UrlShortController.cs
@@ -15,7 +15,17 @@ namespace UrlShort.API.Controllers
             _urlShortService = urlShortService;
         }
 
-        [HttpPost("shorten")]
+        /// <summary>
+        ///     Cria um URL encurtado
+        /// </summary>
+        /// <param name="shortenUrlDto">Dados da URL a ser encurtada</param>
+        /// <returns>Dados da URL encurtada criada</returns>
+        /// <response code="200">URL encurtada criada com sucesso</response>
+        /// <response code="400">Dados inválidos</response>
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponse<UrlShortDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse<UrlShortDto>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse<UrlShortDto>), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Shorten([FromBody] ShortenUrlDto shortenUrlDto)
         {
             var response = await _urlShortService.ShortenUrl(shortenUrlDto);
@@ -28,10 +38,19 @@ namespace UrlShort.API.Controllers
                 return StatusCode(response.StatusCode, response);
         }
 
-        [HttpGet("{shortUrl}")]
-        public async Task<IActionResult> Get(string shortUrl)
+        /// <summary>
+        ///     Obtém uma URL original pela URL encurtada
+        /// </summary>
+        /// <param name="hashUrl">Hash da URL</param>
+        /// <returns>URL original da URL encurtada</returns>
+        /// <response code="302">Redireciona para a URL original</response>
+        /// <response code="400">Dados inválidos</response>
+        [ProducesResponseType(typeof(ApiResponse<string>), StatusCodes.Status302Found)]
+        [ProducesResponseType(typeof(ApiResponse<string>), StatusCodes.Status400BadRequest)]
+        [HttpGet("{hashUrl}")]
+        public async Task<IActionResult> Get(string hashUrl)
         {
-            var response = await _urlShortService.GetShortUrl(shortUrl);
+            var response = await _urlShortService.GetShortUrl(hashUrl);
             
             if (response.StatusCode is >= 200 and < 300)
                 return Redirect(response.Data);
